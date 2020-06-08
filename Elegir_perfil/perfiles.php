ecddf1b873f37d08f8f5a2df1dfdd16f434ba50b
DamaBeth <44103927+DamaBeth@users.noreply.github.com>
2020-06-07 20:36:53 -0500

    Avances

---
+++ Elegir_perfil/perfiles.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>Enviar imagen por AJAX</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="js/subirimagen.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+    <style>
+
+        #main-header {
+	        background: #161515;
+	        color: black;
+	        height: 80px;
+            top: 0px;
+        }	
+        
+        body {
+            padding-top: 50px;
+        }
+
+        .starter-template {
+            padding: 40px 15px;
+            text-align: center;
+        }
+
+        .circular--landscape {
+            display: inline-block;
+            position: relative;
+            width: 150px;
+            height: 150px;
+            border-radius: 50%;
+        }
+
+        .circular--landscape img {
+            width: auto;
+            height: 100%;
+            margin-left: -50px;
+        }
+        img{
+            border-radius: 10px;
+        }
+    </style>
+</head>
+
+<body>
+    <header id = "main-header">
+    </header>
+</body>
+</html>
