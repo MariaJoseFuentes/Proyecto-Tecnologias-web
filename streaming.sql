3756f5cb1aab6d0aee0f31a237c3ee40ad1ecbcb
MariaJoseFuentes <mjfp1506@gmail.com>
2020-06-09 11:11:07 -0500

    Actualizacion de rutas (imagenes)

--- streaming.sql
+++ streaming.sql
@@ -12,7 +12,7 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,N
 -- -----------------------------------------------------
 DROP SCHEMA IF EXISTS `streaming` ;
 
--- -----------------------------------------------------
+-- -----------------------------------------------------
 -- Schema streaming
 -- -----------------------------------------------------
 CREATE SCHEMA IF NOT EXISTS `streaming` DEFAULT CHARACTER SET utf8 ;
@@ -144,27 +144,27 @@ insert into usuario(nombre,foto,ididioma,idclasificacion)
 values("Alejandro","C:/xampp/htdocs/Proyecto-Tecnologias-web/Crear_perfil/fotos/image06033419.jpg",4,4);
 
 insert into pelicula(nombre,duracion,genero,imagen,ididioma,idclasificacion) 
-values("Hancock","1 h 32 min","Fantasía, Comedia","C:/streaming/hancock.jpg",2,3),
-("Harry Potter","2 h 25 min","Fantasía","C:/streaming/harrypotter.jpg",2,1),
-("Las crónicas de Narnia: el león, la bruja y el ropero","1 h 15 min","Fantasía","C:/streaming/narnia.jpg",2,1),
-("Up: Una aventura de altura","1 h 02 min","Fantasía","C:/streaming/up.jpg",1,1),
-("Shrek","1 h 14 min","Fantasía, Comedia","C:/streaming/shrek.jpg",2,2),
-("White chicks","1 h 20 min","Comedia","C:/streaming/whitechicks.jpg",2,2),
-("How to be single","1 h 35 min","Romance","C:/streaming/howtobesingle.jpg",2,3),
-("Toy Story","1 h 01 min","Fantasía","C:/streaming/toystory.jpg",1,1),
-("The ugly truth","1 h 36 min","Comedia romántica","C:/streaming/theuglythruth.gif",2,3),
-("10 cosas que odio de ti","1 h 24 min","Romance","C:/streaming/10things.jpg",1,2);
+values("Hancock","1 h 32 min","Fantasía, Comedia","C:/xampp/htdocs/Proyecto-Tecnologias-web/streaminghancock.jpg",2,3),
+("Harry Potter","2 h 25 min","Fantasía","C:/xampp/htdocs/Proyecto-Tecnologias-web/harrypotter.jpg",2,1),
+("Las crónicas de Narnia: el león, la bruja y el ropero","1 h 15 min","Fantasía","C:/xampp/htdocs/Proyecto-Tecnologias-web/narnia.jpg",2,1),
+("Up: Una aventura de altura","1 h 02 min","Fantasía","C:/xampp/htdocs/Proyecto-Tecnologias-web/up.jpg",1,1),
+("Shrek","1 h 14 min","Fantasía, Comedia","C:/xampp/htdocs/Proyecto-Tecnologias-web/shrek.jpg",2,2),
+("White chicks","1 h 20 min","Comedia","C:/xampp/htdocs/Proyecto-Tecnologias-web/whitechicks.jpg",2,2),
+("How to be single","1 h 35 min","Romance","C:/xampp/htdocs/Proyecto-Tecnologias-web/howtobesingle.jpg",2,3),
+("Toy Story","1 h 01 min","Fantasía","C:/xampp/htdocs/Proyecto-Tecnologias-web/toystory.jpg",1,1),
+("The ugly truth","1 h 36 min","Comedia romántica","C:/xampp/htdocs/Proyecto-Tecnologias-web/theuglythruth.gif",2,3),
+("10 cosas que odio de ti","1 h 24 min","Romance","C:/xampp/htdocs/Proyecto-Tecnologias-web/10things.jpg",1,2);
 
 insert into programa(nombre,temporadas,genero,imagen,ididioma,idclasificacion)
-values("How I met you mother","8","Comedia","C:/streaming/howimet.png",2,3),
-("Dark","2","Drama,Ficción","C:/streaming/dark.jpg",2,3),
-("The Good Doctor","7","Drama médico","C:/streaming/thegooddoctor.jpg",2,2),
-("Escandalosos","2","Animación","C:/streaming/escandalosos.jpg",1,1),
-("Game of Thrones","8","Drama","C:/streaming/gameofthrones.jpg",2,3),
-("Grey's Anatomy","15","Drama","C:/streaming/grey.jpg",2,3),
-("Pretty little liers","7","Misterio","C:/streaming/prettyLittleLiars.jpeg",2,2),
-("Boys Over Flowers","1","Drama,Romance","C:/streaming/boysoverflowers.jpg",2,2),
-("Club de cuervos","4","Comedia","C:/streaming/club.jpg",2,3),
-("Dr. House","8","Drama","C:/streaming/house.jpg",2,2);
+values("How I met you mother","8","Comedia","C:/xampp/htdocs/Proyecto-Tecnologias-web/howimet.png",2,3),
+("Dark","2","Drama,Ficción","C:/xampp/htdocs/Proyecto-Tecnologias-web/dark.jpg",2,3),
+("The Good Doctor","7","Drama médico","C:/xampp/htdocs/Proyecto-Tecnologias-web/thegooddoctor.jpg",2,2),
+("Escandalosos","2","Animación","C:/xampp/htdocs/Proyecto-Tecnologias-web/escandalosos.jpg",1,1),
+("Game of Thrones","8","Drama","C:/xampp/htdocs/Proyecto-Tecnologias-web/gameofthrones.jpg",2,3),
+("Grey's Anatomy","15","Drama","C:/xampp/htdocs/Proyecto-Tecnologias-web/grey.jpg",2,3),
+("Pretty little liers","7","Misterio","C:/xampp/htdocs/Proyecto-Tecnologias-web/prettyLittleLiars.jpeg",2,2),
+("Boys Over Flowers","1","Drama,Romance","C:/xampp/htdocs/Proyecto-Tecnologias-web/boysoverflowers.jpg",2,2),
+("Club de cuervos","4","Comedia","C:/xampp/htdocs/Proyecto-Tecnologias-web/club.jpg",2,3),
+("Dr. House","8","Drama","C:/xampp/htdocs/Proyecto-Tecnologias-web/house.jpg",2,2);
 
 commit;
