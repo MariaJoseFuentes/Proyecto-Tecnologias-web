4ac096a9b0c788e40899848295dcdb45efb20d98
MariaJoseFuentes <mjfp1506@gmail.com>
2020-06-09 12:13:21 -0500

    actualizacion de base de datos2

--- Contenido/mostrarContenido.php
+++ Contenido/mostrarContenido.php
@@ -5,14 +5,15 @@ if(isset($_REQUEST['contenido']))
         $contenido = $contenido = $_REQUEST['contenido'];
         if($contenido== "peliculas")
         {
-            $sql=$db->query("SELECT pelicula.nombre,pelicula.duracion, pelicula.idclasificacion, clasificacion.clasificacion FROM pelicula inner join clasificacion on pelicula.idclasificacion=clasificacion.idclasificacion");
+            $sql=$db->query("SELECT pelicula.nombre,pelicula.duracion, pelicula.idclasificacion, pelicula.imagen, clasificacion.clasificacion FROM pelicula inner join clasificacion on pelicula.idclasificacion=clasificacion.idclasificacion");
             if($sql) {
                 $json = array();
                 while($row = mysqli_fetch_array($sql)) {
                     $json[] = array(
                         'nombre' => $row['nombre'],
                         'duracion' => $row['duracion'],
-                        'clasificacion' => $row['clasificacion']
+                        'clasificacion' => $row['clasificacion'],
+                        'imagen' => $row['imagen']
                     );
                 }
                 $jsonstring = json_encode($json);
@@ -26,7 +27,8 @@ if(isset($_REQUEST['contenido']))
                         $json[] = array(
                             'nombre' => $row['nombre'],
                             'temporadas' => $row['temporadas'],
-                            'genero' => $row['genero']
+                            'genero' => $row['genero'],
+                            'imagen' => $row['imagen']
                         );
                     }
                     $jsonstring = json_encode($json);
